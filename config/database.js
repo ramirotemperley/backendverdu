34fee956dee4e24e28d7896ee4701f5277948a3b
ramirotemperley <130579712+ramirotemperley@users.noreply.github.com>
2024-12-30 15:24:48 -0300

    Actualiza la lógica de validación en rutas de ventas

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Verduleria', {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Verduleria', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
